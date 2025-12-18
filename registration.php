89b6f7cd30c6a1cae815118291f5e5adc1916ef0
Rahul Barot <88397713+DevAwesome16@users.noreply.github.com>
2025-12-18 12:27:18 +0530

    initial commit

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © Avenyra. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Avenyra_Base',
+    __DIR__
+);
+
